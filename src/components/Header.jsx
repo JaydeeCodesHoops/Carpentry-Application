b4144f4836bfed71fec5f19f7800206d0391b6e3
Jaden Van Schalkwyk <vanschalkwykjaden10@gmail.com>
2025-09-27 02:08:55 +0200

    Updated the navs in the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import Jays from '../../public/images/JaysWoodworksLogo.png';
+import { useNavigate } from "react-router-dom";
 import '../css/Header.css';
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    const handleHome = () => {
+        navigate('/');
+    }
+    const handleDesigns = () => {
+        navigate('/design');
+    }
+    const handleSocials = () => {
+        navigate('/socials');
+    }
+    const handleContact = () => {
+        navigate('/contact');
+    }
+    const handleInvoice = () => {
+        navigate('/invoice');
+    }
+
     return (
         <>
             <div className="headerDiv">
                 <div className="img-Name">
-                    <img className="image" src={Jays} />
+                    <img onClick={handleHome} className="image" src={Jays} />
                     <h2 className="companyName">Jay's Woodworks</h2>
-                    {/* <hr style={{ height: "1px", width: "80vw", border: "none", backgroundColor: "gray" }} /> */}
                 </div>
                 <div className="navBtns">
-                    <p>Home</p>
-                    <p>Designs</p>
-                    <p>Socials</p>
-                    <p>Contact Us</p>
-                    <p>Invoice</p>
+                    <p onClick={handleHome}>Home</p>
+                    <p onClick={handleDesigns}>Designs</p>
+                    <p onClick={handleSocials}>Socials</p>
+                    <p onClick={handleContact}>Contact Us</p>
+                    <p onClick={handleInvoice}>Invoice</p>
                 </div>
             </div>
+            {/* <hr style={{ height: "1px", width: "80vw", border: "none", backgroundColor: "gray" }} /> */}
         </>
     );
 }
